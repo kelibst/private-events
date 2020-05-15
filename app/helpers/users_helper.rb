eb8e2326105960c8dbe4dd1e49e30431d05d7439
kelibst <kbooster17@gmail.com>
2020-05-15 04:14:13 +0000

    added navigation bar and necessary paths

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,4 +3,10 @@ module UsersHelper
         event = Event.find_by(id: params[:id])
         return !event.invitations.where(attendee: current_user).exists?
     end
+
+    def event_not_expired?
+        event = Event.find_by(id: params['id'])
+        is_expired = (event.date <= DateTime.now)? true : false
+        return !is_expired
+    end
 end
