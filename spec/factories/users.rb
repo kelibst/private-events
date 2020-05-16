6c5780de6a1903391c729d31cc5c9fca6811fc96
kelibst <kbooster17@gmail.com>
2020-05-16 10:18:28 +0000

    added rspec-test

---
+++ spec/factories/users.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+    factory :user do
+    sequence(:username, 100) { |n| "person#{n}" }
+    sequence(:email, 100) { |n| "person#{n}@example.com" }
+end
+end
--- spec/requests/sessions_request_spec.rb
+++ spec/requests/sessions_request_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
+require 'capybara/rspec'
 
-RSpec.describe "Sessions", type: :request do
 
+
+describe 'Signin authentication test', type: :feature do
+ 
+   user = FactoryGirl.create(:user)
+
+    it 'ensures sign in is working' do
+        
+      visit '/login'
+      within('form') do
+        fill_in 'username', :with => user.username
+        fill_in 'email', :with => user.email
+      end
+      click_button 'Sign in'
+
+      expect(page).to have_content 'You have successfully logged in.'
+      
+    end
+end
+
+
+
+RSpec.describe 'checking sign up is accessible', type: :system do
+  describe 'signup page is accessible' do
+    it 'shows the right content from signup page' do
+      visit '/signup'
+      expect(page).to have_content('Account')
+    end
+  end
 end
